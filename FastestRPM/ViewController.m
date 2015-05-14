db5d46bb3305747500fd98b21c03734738e27cec
Jacy Wang <jacywj@gmail.com>
2015-05-14 11:52:56 -0700

    Add UIPanGestureRecognizer

--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
+#define degreesToRadians(degrees) (M_PI * degrees / 180.0)
+
 @interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UIImageView *needle;
+
 
 @end
 
@@ -17,11 +22,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.needle.transform = CGAffineTransformIdentity;
+    self.needle.transform = CGAffineTransformMakeRotation(degreesToRadians(135));
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(move:)];
+    panGesture.maximumNumberOfTouches = 1;
+    
+    [self.view addGestureRecognizer:panGesture];
+    
+
+//    myImage.transform = CGAffineTransformRotate(myImage.transform, degreesToRadians(90));
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)move:(UIPanGestureRecognizer *)sender {
+    
 }
 
 @end
